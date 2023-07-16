da769fec391bd10bb04fe100791661d19b1f77cc
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-07-16 08:54:01 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,32 +1,25 @@
+# Don't Edit
+
 import os
 
 from dotenv import load_dotenv
 load_dotenv()
 
-def is_enabled(value, default):
-    if value.lower() in ["true", "yes", "1", "enable", "y"]:
-        return True
-    elif value.lower() in ["false", "no", "0", "disable", "n"]:
-        return False
-    else:
-        return default
 
 # Mandatory variables for the bot to start
-API_ID = int(os.environ.get("API_ID")) #API ID from https://my.telegram.org/auth
-API_HASH = os.environ.get("API_HASH") #API Hash from https://my.telegram.org/auth
-BOT_TOKEN = os.environ.get("BOT_TOKEN") # Bot token from @BotFather
-ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split(",")] if os.environ.get("ADMINS") else []
-
-DATABASE_NAME = os.environ.get("DATABASE_NAME")
-DATABASE_URL = os.environ.get("DATABASE_URL") # mongodb uri from https://www.mongodb.com/
-OWNER_ID =  int(os.environ.get("OWNER_ID")) # id of the owner
-ADMINS.append(OWNER_ID) if OWNER_ID not in ADMINS else []
-
+API_ID = int(os.getenv("API_ID", "23990433"))
+API_HASH = os.environ.get("API_HASH", "e6c4b6ee1933711bc4da9d7d17e1eb20")
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6390467508:AAEPjEY6IZbkAHx6LtGx3PYiX8J_A-EtNPY")
+ADMINS = [int(i.strip()) for i in os.environ.get("ADMINS").split("5821871362")] if os.environ.get("ADMINS") else []
+ADMIN = ADMINS
+DATABASE_NAME = os.environ.get("DATABASE_NAME", "nakflixbot")
+DATABASE_URL = os.getenv("DATABASE_URL", "mongodb+srv://nakflixbot:alpha3720@cluster0.qgybxbu.mongodb.net/?retryWrites=true&w=majority") 
+OWNER_ID =  int(os.environ.get("OWNER_ID", "5821871362")) 
+ADMINS.append(5821871362) if OWNER_ID not in ADMINS else []
+ADMINS.append(5821871362)
 #  Optionnal variables
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL")) # log channel for information about users
-UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", False) # For Force Subscription
-BROADCAST_AS_COPY = is_enabled((os.environ.get('BROADCAST_AS_COPY', "False")), False) # true if forward should be avoided
-WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '') # image when someone hit /start
-LINK_BYPASS = "True" 
-
-
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001870015374")) 
+UPDATE_CHANNEL = os.environ.get("UPDATE_CHANNEL", "SK_MoviesOffl") # For Force Subscription
+BROADCAST_AS_COPY = os.environ.get('BROADCAST_AS_COPY', "True") # true if forward should be avoided
+WELCOME_IMAGE = os.environ.get("WELCOME_IMAGE", '') # image when someone hit /start # image when someone hit /start
+LINK_BYPASS = "False"
